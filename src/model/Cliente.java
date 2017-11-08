d1bc580098581413aeb115dbe75943c1c0c6877c
Junior Santos <jsantos.class@gmail.com>
2017-11-08 16:07:20 -0300

    Corrigido o bug da listagem dos pacotes

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -3,6 +3,8 @@ package model;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.time.LocalDateTime;
+
 import io.Servidor;
 import javafx.concurrent.Task;
 import javafx.scene.control.ListView;
@@ -13,6 +15,7 @@ public class Cliente extends Task<Void>{
 	private ListView<String> list_packets;
 	
 	public Cliente(ListView<String> list_packets, Socket socket) {
+		this.list_packets=list_packets;
 		this.socket= socket;
 	}
 
@@ -26,16 +29,15 @@ public class Cliente extends Task<Void>{
 		while(true) {
 			DataInputStream in= new DataInputStream(socket.getInputStream());
 			String mensagem=in.readUTF();
-			if(mensagem.split("¨¨")[1].equals("REMOVER$chat"))
+			if(mensagem.split("Â¨Â¨")[1].equals("REMOVER$chat"))
 				Servidor.conectados.remove(socket);
 			else
 				Servidor.Broadcast(mensagem);	
-			//in.read
-			//StringBuffer sb= new StringBuffer();
-			/*while(in.read()!=-1) {
-				sb.append(in.readByte());
-			}*/
-			//list_packets.getItems().add("[".concat(LocalDateTime.now().toString()).concat("] Pacote recebido de ".concat(socket.getLocalAddress().getHostAddress())).concat(sb.toString()));
+			StringBuffer sb= new StringBuffer();
+			for(byte b:mensagem.getBytes())
+				sb.append(String.valueOf(b).concat(" "));
+			list_packets.getItems().add("[".concat(LocalDateTime.now().toString()).concat("]").concat("Recebemos o pacote :"
+					.concat(sb.toString())));
 		}
 	}
 }
