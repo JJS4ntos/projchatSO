837513cc85550d6318077d9705073f99a62b059a
Junior Santos <jsantos.class@gmail.com>
2017-11-13 13:42:24 -0300

    Troca de informações agora via ObjectStream

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,11 +1,12 @@
 package model;
 
-import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.time.LocalDateTime;
 
 import io.Servidor;
+import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.scene.control.ListView;
 
@@ -27,17 +28,17 @@ public class Cliente extends Task<Void>{
 	protected Void call() throws IOException {
 		// TODO Auto-generated method stub
 		while(true) {
-			DataInputStream in= new DataInputStream(socket.getInputStream());
+			ObjectInputStream in= new ObjectInputStream(socket.getInputStream());
 			String mensagem=in.readUTF();
-			if(mensagem.split("Â¨Â¨")[1].equals("REMOVER$chat"))
+			if(mensagem.split("¨¨")[1].equals("REMOVER$chat"))
 				Servidor.conectados.remove(socket);
 			else
 				Servidor.Broadcast(mensagem);	
 			StringBuffer sb= new StringBuffer();
 			for(byte b:mensagem.getBytes())
 				sb.append(String.valueOf(b).concat(" "));
-			list_packets.getItems().add("[".concat(LocalDateTime.now().toString()).concat("]").concat("Recebemos o pacote :"
-					.concat(sb.toString())));
+			Platform.runLater(()->list_packets.getItems().add("[".concat(LocalDateTime.now().toString()).concat("]").concat("Recebemos o pacote :"
+					.concat(sb.toString()))));
 		}
 	}
 }
